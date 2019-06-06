1dd5f2b53fa7ca52cec27c41fb7cdb85c2393f74
Alok Singh <8325708+alok@users.noreply.github.com>
2019-06-05 23:45:13 -0700

    Work on Alfred 4

--- scripts/first_run_check.sh
+++ scripts/first_run_check.sh
@@ -1,16 +1,16 @@
 # -----------------------------------------
-# EggTimer 2 for Alfred 2
+# EggTimer 2 for Alfred 3+
 # by Carl Smith (@CarlosNZ)
 # -----------------------------------------
 
 #First run check
-if [ ! -e "$HOME/Library/Application Support/Alfred 3/Workflow Data/carlosnz.eggtimer2/version" ]; then
+if [ ! -e "$HOME/Library/Application Support/Alfred/Workflow Data/carlosnz.eggtimer2/version" ] && [ ! -e "$HOME/Library/Application Support/Alfred 3/Workflow Data/carlosnz.eggtimer2/version" ]; then
 	./scripts/first_install.sh
 	exit
 fi
 
 
-ver=$(cat "$HOME/Library/Application Support/Alfred 3/Workflow Data/carlosnz.eggtimer2/version")
+ver=$(cat "$HOME/Library/Application Support/Alfred/Workflow Data/carlosnz.eggtimer2/version")
 if [ $ver -lt 205 ]; then
 	./scripts/first_install.sh
 fi
